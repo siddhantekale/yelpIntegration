205f781214cb8e0e7c0b2e3effc4fb300bd533d1
Siddhant <sekale@purdue.edu>
2017-03-15 03:08:46 -0400

    Formatting and Indentation - minor

--- api/resolver.js
+++ api/resolver.js
@@ -1,6 +1,8 @@
 var request = require('request');
 var _ = require('underscore');
 var Yelp = require('yelp');
+
+//Yelp Authentication: https://www.yelp.com/developers/v2/manage_api_keys
 var yelp = new Yelp({
   consumer_key: process.env.consumer_key,
   consumer_secret: process.env.consumer_secret,
@@ -17,12 +19,21 @@ module.exports = function(req, res) {
 function yelpReviewSearch(id, req, res){
   yelp.business(id)
   .then(function (data) {
-    var width = data.rating_img_url_small.width > 200 ? 200: data.rating_img_url.width;
+    var width = data.rating_img_url_small.width > 200 ? 
+      200: data.rating_img_url.width;
+    //Location attribute variables
     var businessName = '<a href=' + data.url + '>' +  data.name  + '</a>';
-    var ratingsStarHtmlTag = '<img style="max-width:100%;" src="' + data.rating_img_url_small + '" width="' + width + '"/>';
+
+    var ratingsStarHtmlTag = '<img style="max-width:100%;" src="' + 
+      data.rating_img_url_small + '" width="' + width + '"/>';
+
     //converts the address into a queryable form
-    var retAddr = getAddressString(data.location.address + '', data.location.city + '') + '';
-    var embedMapAddress = '<a href =\"https://www.google.com/maps/?q=' + retAddr + '\">' +  data.location.address  + '</a>';
+    var retAddr = getAddressString(data.location.address + '', 
+        data.location.city + '') + '';
+
+    var embedMapAddress = '<a href =\"https://www.google.com/maps/?q=' + 
+      retAddr + '\">' +  data.location.address  + '</a>';
+
     res.json({
     body: businessName + ' ' + ratingsStarHtmlTag + ' ' + embedMapAddress,
     raw: true
@@ -32,9 +43,7 @@ function yelpReviewSearch(id, req, res){
     console.error(err);
   });
 }
-
 //Converts address to queryable form for e.g. 420 Northwestern Ave = 420+Northwestern+Ave
-
 function getAddressString(addressString, cityName){
   var retAddr = '';
   var tempAddrArray = addressString.split(" ");  
